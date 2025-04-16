c6992dee30f44a3fc37ddb6e4d72d486975421c5
tidso-cspbb <tidso@cspbb.org>
2025-04-15 21:19:32 -0700

    Update includes.js

--- js/includes.js
+++ js/includes.js
@@ -1,3 +1,16 @@
+window.addEventListener('DOMContentLoaded', () => {
+  includeHTML('header', 'includes/header.html');
+  includeHTML('footer', 'includes/footer.html');
+});
+
+function includeHTML(tagId, filePath) {
+  fetch(filePath)
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById(tagId).innerHTML = data;
+    });
+}
+
 function handleDropdownClick(event) {
   const isTouch = window.matchMedia('(hover: none)').matches;
   if (!isTouch) {
